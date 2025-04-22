d7dfc715cfae127253a14ca49ca2c50c0fc5d6eb
raulmj <rauljesus24@gmail.com>
2025-04-22 10:14:43 +0200

    Escritura de datos en archivo

---
+++ clase_6.py
@@ -0,0 +1,17 @@
+x = 3
+y = 2
+z = 2.05
+e = 3e2
+is_true = True
+is_false= False
+#____________________________________
+print("El resultado de x + y es: ", x + y)
+print(f"El resultado de x + y es: {x + y}")
+print("El resultado de x + z es: ", x + z)
+print("la potencia de: ", x ,"elevada a: ", y, "es: ", e)
+print("El resultado de x + y es: ", x + y,"\nY el resultado de x - y es: ",x - y)
+print(is_true)
+print(type(z))
+print(type(is_true))
+print(int(z))
+print(float(x))
